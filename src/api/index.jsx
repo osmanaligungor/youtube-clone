764e0e2bb06c71918a12e72109254e2fc6a4a304
osmanaligungor <o.aligngr3232@gmail.com>
2025-02-10 15:27:03 +0300

    add env variables into env file

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://yt-api.p.rapidapi.com",
+  baseURL: import.meta.env.VITE_BASE_URL,
   params: {
     geo: "TR",
     lang: "tr",
   },
   headers: {
-    "x-rapidapi-key": "1efc9f745fmsh71ff1ec50ab9738p1ab034jsnbef54d42b566",
-    "x-rapidapi-host": "yt-api.p.rapidapi.com",
+    "x-rapidapi-key": import.meta.env.VITE_X_RAPID_API_KEY,
+    "x-rapidapi-host": import.meta.env.VITE_X_RAPID_APIHOST_URL,
   },
 });
 
